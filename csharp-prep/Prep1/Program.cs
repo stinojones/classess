e4a9fee1cab9c58efb870bec0206cf1d5c7fc26e
Austin Jones <stinobuddy@gmail.com>
2023-04-22 17:12:42 -0600

    Prep1 and Prep2 Complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,184 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+// console.Write makes it so there is no new line made after the text and you can keep it going
+        
+        Console.Write("so if I wanted to keep writing on this line and not create a new one ");
+        Console.WriteLine("I could do so with console.write above");
+
+// console.WriteLine makes it possible to have a new line underneath made after text or anything is written
+
+        Console.WriteLine("but if I did want a new line to start then");
+        Console.WriteLine("I would use console.writeline");
+
+// this is how you write out an input statement with the answer right next to the question
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+
+// input statement if you want answer underneath
+
+        Console.WriteLine("What is your favorite food? ");
+        string food = Console.ReadLine();
+
+        int x = 31;
+        int y = 22;
+        int z = 30;
+
+// starting the if statement you must use parenthesis and you don't need a semicolon to start it
+        
+        if (x > y)
+        {
+            string statement = "Greater";
+
+            // you can add variable into the string by using $    like so
+        Console.WriteLine($"the number of x is {statement} than y");  
+        }
+        
+        
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+
+
+        // how to indent your if statements
+        if (x > y)
+        {
+            if (x > z)
+            {
+                Console.WriteLine("greater than both");
+            }
+        }
+    
+
+
+
+        // if and else statements
+
+        
+
+        if (x > y)
+        {
+            Console.WriteLine("greater than");
+        }
+        else
+        {
+            Console.WriteLine("less than");
+        }
+
+
+        // elif is written as else if in C#
+        if (x > y)
+        {
+            Console.WriteLine("greater than y");
+        }
+        else if (x > z)
+        {
+            Console.WriteLine("greater than z");
+        }
+        else
+        {
+            Console.WriteLine("less than both");
+        }
+
+
+        // things like == and != are used in c#
+
+        string name = "John";
+
+        string favoriteColor = "string";
+        if (name == "John")
+        {
+            Console.WriteLine("The name is John");
+        }
+
+        if (color != favoriteColor)
+        {
+            Console.WriteLine("That color is not my favorite");
+        }
+
+
+
+
+        // and command is written as && and the or command is ||
+
+
+
+        if (name == "Peter" || name == "James" || name == "John")
+        {
+            Console.WriteLine("This is a biblical name.");
+        }
+
+        if (firstName == "Brigham" && lastName == "Young")
+        {
+            Console.WriteLine("Welcome Brother Brigham!");
+        }
+
+        if (!(name == "Peter" || name == "James" || name == "John"))
+        {
+            Console.WriteLine("This is a not one of those three");
+        }
+
+
+
+
+
+
+        // you can change strings to int's mid code by using int.Parse(userInput)
+        // If the user typed in something other than a number, this would result in type of error called a Runtime Exception.
+        Console.Write("What is your favorite number? ");
+        string userInputed = Console.ReadLine();
+        int number = int.Parse(userInputed);
+
+
+
+
+        // Similarly, an integer can be converted to a string using the .ToString() function of the variable.
+        int numbers = 42;
+        string textVersion = numbers.ToString();
+
+
+        
+        
+        
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
+
+
+
+    
     }
 }
+
+
+
+
+
+
+
+
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+          // this is the Assignment of the grade calculator
+
+
+        Console.Write("What is your grade percentage in the class? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        int gradeRemainder = grade % 10;
+
+        string letter = "";
+        string sign = "";
+
+       
+        if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 80 && grade <90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 90)
+        {
+            letter = "A";
+        }
+        if (letter == "D" || letter == "C" || letter == "B")
+        {
+            if (gradeRemainder >=7) 
+            {
+                sign = "+";
+            }
+            else if (gradeRemainder < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"You're Final Grade is a {letter}{sign}");  
+
+        if (grade >= 70)    
+        {
+            Console.WriteLine("Great Job! You passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, You did not pass the class, keep studying!");
+        }
+
+        
+           
+
+        
+
+
+
+
+
     }
+
+
 }
